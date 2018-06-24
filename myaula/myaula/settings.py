21f8fa536fea4f1053b5921f9eceab6bece8d864
Caio Oliveira <caiomcoliveira@gmail.coml>
2018-06-24 12:14:33 -0300

    Configurando deploy

--- myaula/myaula/settings.py
+++ myaula/myaula/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '@nli5#i3ch2ktvw7!(79*+al!-t9nlobi1eb)@y1iz!@avr!my'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
--- src/api/routes.ts
+++ src/api/routes.ts
@@ -1,11 +1,11 @@
 export namespace Api {
-    export const rootPath = "http://localhost:8100/api/"
+    export const rootPath = "http://192.168.0.3:8000/api/"
     export const turmas: string = rootPath + "turmas/";
     export const login: string = rootPath + "login/";
 
     export const entrarTurma: string = rootPath+ "adicionar/";
     export const cadastrar: string = rootPath+ "cadastrar/";
-    export const usuarioPorMatricula = (matricula: string) => `http://localhost:8100/api/usuario/${matricula}/`;
+    export const usuarioPorMatricula = (matricula: string) => `http://192.168.0.3:8000/api/usuario/${matricula}/`;
     export const questionario: string = rootPath+ "questionario/";
     
 
